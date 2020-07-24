29393079d9a33275756ec4b3b077eec473f5d2ee
Tiron <ole_g92@bk.ru>
2020-07-24 16:19:46 +0300

    сбор логов

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const cardsRoute = require('./routes/cards');
 const usersRoute = require('./routes/users');
 const { login, createUser } = require('./controllers/users');
 require('dotenv').config();
 const { auth } = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-err');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,6 +23,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
+app.use(requestLogger);
+
 app.post('/signup', createUser);
 app.post('/signin', login);
 app.use('/cards', auth, cardsRoute);
@@ -28,6 +32,10 @@ app.use('/users', auth, usersRoute);
 app.all('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваемый ресурс не найден'));
 });
+
+app.use(errorLogger);
+
+app.use(errors());
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
