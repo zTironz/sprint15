b4834fa9e63a2fb909f297ff932097a6104ee517
Tiron <ole_g92@bk.ru>
2020-07-24 14:45:39 +0300

    Валидация приходящих на сервер данных

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const cardsRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
 cardsRouter.get('/', getCards);
 
-cardsRouter.post('/', createCard);
+cardsRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}), createCard);
 
-cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const usersRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const { getUsers, getUser } = require('../controllers/users');
 
 usersRouter.get('/', getUsers);
 
-usersRouter.get('/:userId', getUser);
-
-// usersRouter.post('/', createUser);
+usersRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24),
+  }),
+}), getUser);
 
 module.exports = usersRouter;
